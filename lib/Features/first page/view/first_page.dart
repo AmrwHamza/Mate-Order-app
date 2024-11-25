f16f2a6e7c71b611f945af751710ea1167dee42e
tarek-alasfour-111 <tarekalasfour123@gmail.com.com>
2024-11-25 08:43:47 -0800

    edits on folders

--- lib/Features/first page/view/first_page.dart
+++ lib/Features/first page/view/first_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:mate_order_app/Features/splash/views/widget/custom_botton.dart';
+import 'package:mate_order_app/Features/first%20page/view/widget/custom_botton.dart';
 import 'package:mate_order_app/constants.dart';
 import 'package:mate_order_app/core/utils/assets.dart';
 
